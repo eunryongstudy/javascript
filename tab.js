a8e581bad0840fa60273fe5308f426711d28308c
eunryong <studyeunryong@gmail.com>
2022-05-26 12:27:21 +0900

    black bg

--- tab.js
+++ tab.js
@@ -1,6 +1,6 @@
 var 버튼 = $('.tab-button')
 
-for(let i = 0; i < 3; i++){
+for(let i = 0; i < $('.tab-button').length; i++){
     버튼.eq(i).on('click', function(){
         버튼.removeClass('orange');
         버튼.eq(i).addClass('orange');

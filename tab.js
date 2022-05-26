202ba8e200aef0361d2f81cc7a5c81fc45d1915e
eunryong <studyeunryong@gmail.com>
2022-05-26 12:56:42 +0900

    이벤트 버블링 dataset

--- tab.js
+++ tab.js
@@ -1,10 +1,19 @@
 var 버튼 = $('.tab-button')
 
-for(let i = 0; i < $('.tab-button').length; i++){
-    버튼.eq(i).on('click', function(){
+// for(let i = 0; i < $('.tab-button').length; i++){
+//     버튼.eq(i).on('click', function(){
+//         탭열기(i);
+//     })
+// }
+
+function 탭열기(num){
         버튼.removeClass('orange');
-        버튼.eq(i).addClass('orange');
+        버튼.eq(num).addClass('orange');
         $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })
+        $('.tab-content').eq(num).addClass('show');
 }
+
+
+$('.list').click(function(e){
+        탭열기(e.target.dataset.id);
+})

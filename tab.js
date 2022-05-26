e374610b695ba7f3e949be8faded92d02df7ec76
eunryong <studyeunryong@gmail.com>
2022-05-26 17:31:25 +0900

    list

--- tab.js
+++ tab.js
@@ -19,6 +19,21 @@ $('.list').click(function(e){
 })
 
 var car = ['소나타', 50000, 'white'];
-var car2 = {name:'소나타', price:50000, color:'white'}
+var car2 = {name : '소나타', price : [50000, 3000, 200], color : 'white'}
 $('.car-title').html(car2.name)
-$('.car-price').html(car2.price)
+$('.car-price').html(car2.price[0])
+
+var products = [
+    { id : 0, price : 70000, title : 'Blossom Dress' },
+    { id : 1, price : 50000, title : 'Springfield Shirt' },
+    { id : 2, price : 60000, title : 'Black Monastery' }
+  ];
+
+// $('.card-body h5')[0].html = products[0]['title'];
+
+
+for(let i = 0; i < products.length; i++)
+{
+    document.querySelectorAll('.card-body h5')[i].innerHTML = products[i].title;
+    document.querySelectorAll('.card-body p')[i].innerHTML = '가격 : '+ products[i].price;
+}

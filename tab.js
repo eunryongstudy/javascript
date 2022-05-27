9c32cc7de5dcdb984a53f44fb0870c7e8c2e7460
eunryong <studyeunryong@gmail.com>
2022-05-27 17:44:48 +0900

    html추가 및 배열 반복문

--- tab.js
+++ tab.js
@@ -38,11 +38,32 @@ var products = [
 //     document.querySelectorAll('.card-body p')[i].innerHTML = '가격 : '+ products[i].price;
 // }
 $('.form-select').eq(0).on('input',function(){
-
+    var pants = [28, 30, 32];
+    var shirts = [95, 100, 105];
     var value = $('.form-select').eq(0).val();
     if(value == '셔츠'){
     $('.form-select').eq(1).removeClass('hide');
-}   else{
+    $('.form-select').eq(1).html('');
+    shirts.forEach(function(a){
+        $('.form-select').eq(1).append(`<option>${a}</option>`);
+    })
+}   else if(value == '바지'){
+    $('.form-select').eq(1).removeClass('hide');
+    $('.form-select').eq(1).html('');
+    
+    pants.forEach(function(a){
+        $('.form-select').eq(1).append(`<option>${a}</option>`);
+    })
+}
+else{
     $('.form-select').eq(1).addClass('hide');
 }
 })
+
+
+// var a = document.createElement('p');
+// a.innerHTML = '안녕';
+// document.querySelector('#test').appendChild(a);
+
+var 템플릿 = '<p>안녕</p>'
+document.querySelector('#test').insertAdjacentHTML('beforeend', 템플릿);
